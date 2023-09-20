e11d49179ca4dc04b79489c71b07dc13789a6670
Niraj Patel <nirajp82@yahoo.com>
2023-09-19 21:17:23 -0400

    Pending change

--- Microservice/CDK/Lambda/lib/lambda-stack.ts
+++ Microservice/CDK/Lambda/lib/lambda-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +13,11 @@ export class LambdaStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'LambdaQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const helloFunc = new Function(this, "HelloHander", {
+        runtime =  Runtime.NODEJS_14_X,
+        code = Code.fromAsset("function"),
+        
+    });
   }
 }
